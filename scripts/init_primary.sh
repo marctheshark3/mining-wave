cd7df36f289add368d4dba296551df5446c36a4b
Marc Mailloux <marctheshark333@gmail.com>
2024-09-04 10:30:11 -0600

    initial commit for mirroring mc db to secondary server

---
+++ scripts/init_primary.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start the primary server
+docker-compose -f ../primary_server/docker-compose.yml up -d
+
+# Wait for the primary server to be ready
+sleep 10
+
+# Create replication user
+docker exec postgres_primary psql -U ***REMOVED***-c "CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';"
+
+echo "Primary server initialized. Please update the PRIMARY_IP in the secondary_server/conf/recovery.conf file."
