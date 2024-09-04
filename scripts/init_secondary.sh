42c2e6bcb4c32d3d06e5510661f1af391de1989d
Marc Mailloux <marctheshark333@gmail.com>
2024-09-04 10:52:52 -0600

    password bug

--- scripts/init_secondary.sh
+++ scripts/init_secondary.sh
@@ -6,8 +6,15 @@ if [ -z "$PRIMARY_IP" ]; then
     exit 1
 fi
 
+# Ensure REPLICATOR_PASSWORD is set
+if [ -z "$REPLICATOR_PASSWORD" ]; then
+    echo "Please set the REPLICATOR_PASSWORD environment variable before running this script."
+    exit 1
+fi
+
 # Update recovery.conf with the primary server IP
 sed -i "s/PRIMARY_IP/$PRIMARY_IP/" ../secondary_server/conf/recovery.conf
+sed -i "s/replicator_password/$REPLICATOR_PASSWORD/" ../secondary_server/conf/recovery.conf
 
 # Start the replica server
 docker-compose -f ../secondary_server/docker-compose.yml up -d replica
@@ -25,6 +32,7 @@ sudo rm -rf /var/lib/docker/volumes/secondary_server_replica_data/_data/*
 docker run --rm \
     -v secondary_server_replica_data:/var/lib/postgresql/data \
     --network host \
+    -e PGPASSWORD=$REPLICATOR_PASSWORD \
     postgres:13 \
     pg_basebackup -h $PRIMARY_IP -D /var/lib/postgresql/data -P -U replicator
 
