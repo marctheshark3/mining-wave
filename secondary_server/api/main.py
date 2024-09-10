87a12997d6ff68d1a87d2fa583fa4bfa6329f112
Marc Mailloux <marctheshark333@gmail.com>
2024-09-10 10:48:07 -0600

    api adjustment for query

--- secondary_server/api/main.py
+++ secondary_server/api/main.py
@@ -120,7 +120,7 @@ def get_address_column(table_name: str) -> str:
         "balance_changes": "address",
         "payments": "address",
         "minerstats": "miner",
-        "block", "miner",
+        "blocks": "miner",
     }
     return address_columns.get(table_name, "address")
 
