b510a5945b0d742ce0196cdf0bee84635c5da10d
Marc Mailloux <marctheshark333@gmail.com>
2024-12-03 09:35:07 -0500

    changing tooling fileS

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ load_dotenv()
 class Settings(BaseSettings):
     DB_HOST: str = os.getenv("DB_HOST", "")
     DB_PORT: str = os.getenv("DB_PORT", "5432")
-    POSTGRES_USER: str = os.getenv("DB_USER")
-    POSTGRES_PASSWORD: str = os.getenv("DB_PASSWORD")
-    POSTGRES_DB: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_NAME: str = os.getenv("DB_NAME")
     REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost")
 
     class Config:
